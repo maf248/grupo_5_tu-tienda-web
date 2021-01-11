358723f5c022e7d0fbd6d97cf9a1aabf26e49acf
maf248 <maf_248@hotmail.com>
2021-01-11 16:19:46 -0300

    Saque console.logs que habian quedado de prueba

--- src/middlewares/rememberMiddleware.js
+++ src/middlewares/rememberMiddleware.js
@@ -6,8 +6,6 @@ const users = JSON.parse(fs.readFileSync(usersDir, 'utf-8'));
 
 function rememberMiddleware(req, res, next) {
   
-    console.log(req.cookies.recordame);
-    console.log(req.session.user);
   if(req.cookies.recordame != undefined && req.session.user == undefined) {
         users.forEach(user => {
             if (req.cookies.recordame == user.email) {
