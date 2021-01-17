964eed8c6e6162494e718d541ff8f4b2dd796883
maf248 <maf_248@hotmail.com>
2021-01-16 23:30:57 -0300

    Agregue un mensaje de confirmacion previo al borrado de cuenta de usuario, para mayor seguridad y utilidad adecuada a su funcion

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -10,7 +10,6 @@ const users = JSON.parse(fs.readFileSync(usersDir, 'utf-8'));
 var loginMailValue = null;
 var loginPassValue = null;
 
-
 const usersController = {
     login: function(req, res, next) {
         if (req.session.user != undefined) {
@@ -99,6 +98,7 @@ const usersController = {
         if (req.session.user != undefined) {
             
             if  (req.params.id == req.session.user.id){
+                
                 return res.render('./users/profile');
             } else {
                 res.redirect('/users/profile/' + req.session.user.id);
@@ -129,18 +129,16 @@ const usersController = {
         if (req.body.category != "") {
             users[req.params.id -1].category = req.body.category;
         }
-
         
 
         /*-----Guardamos datos en el users.json-----*/
-
         const usersJSON = JSON.stringify(users);
 		fs.writeFileSync(usersDir, usersJSON);
 		res.redirect('/users/profile/'+ req.params.id);
 
     },
     photoUpdate: function (req, res, next) {
-        
+        /*---Aqui se guarda el nombre del archivo del nuevo avatar---*/
         users[req.params.id -1].image = req.files[0].filename;
 
         const usersJSON = JSON.stringify(users);
@@ -149,10 +147,10 @@ const usersController = {
         res.redirect('/users/profile/'+ req.params.id)
     },
     logout: function (req, res, next) {
-
+        /*---Aqui se resetean valores de mensajes de error----*/
         loginMailValue = null;
         loginPassValue = null;
-
+        /*---Aqui se borra la cookie, luego se cierra la session----*/
         res.cookie('recordame', '', {maxAge: 0});
 
         delete req.session.user;
@@ -160,22 +158,23 @@ const usersController = {
         res.redirect('/');
     },
     delete: function(req, res, next){
+        /*---Aquí se borra el usuario del array dentro de la variable users----*/
         users.splice(req.params.id-1, 1)
+         /*---Aquí se corrigen los ID de los usuarios restantes para evitar errores----*/
         for(let i=0; i <users.length; i++){
-           
             users[i].id = i+1 
-
-           
         }
+        /*---Aquí se guardan los cambios en el JSON----*/
         const usersJSON = JSON.stringify(users);
         fs.writeFileSync(usersDir, usersJSON);
+        /*---Aquí se resetean valores de mensajes, se borra la cookie y se cierra session----*/
         loginMailValue = null;
         loginPassValue = null;
 
         res.cookie('recordame', '', {maxAge: 0});
 
         delete req.session.user;
-
+        /*---Aquí se redirije a inicio----*/
         res.redirect('/');
     }
 }
