f706df115c5a0a707989083e48e419282e19436a
maf248 <maf_248@hotmail.com>
2021-02-01 16:42:48 -0300

    Completamos el modelo de User entre todos durante un Zoom

---
+++ src/database/models/User.js
@@ -0,0 +1,72 @@
+module.exports = (sequelize, DataTypes) => {
+
+const cols = {
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    hash_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    image: {
+        type: DataTypes.STRING,
+        defaultValue: NULL
+    },
+    role: {
+        type: DataTypes.ENUM('admin', 'user'),
+        defaultValue: sequelize.literal('user')
+    },
+    product_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    },
+    category_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('NOW()')
+    },
+    updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('NOW()')
+    }
+}
+
+const config = {
+    tableName: 'users',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+}
+
+const User = sequelize.define("User", cols, config);
+
+return User;
+
+}
+
+// Porque sibarita es tan rica??
