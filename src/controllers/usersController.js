b14098bb53760bcfeae183487ea419bbb61a76de
Totoblak90 <tobias.blaksley@hotmail.com>
2021-02-07 14:08:20 -0300

    modifique el controlador de usuarios en el metodo de editar usuarios para que al editar el usuario modifique la DB

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -100,51 +100,71 @@ const usersController = {
         /*---Se chequean los inputs. Si no hay errores los guarda---*/
         if (errors.isEmpty()) {
             /*---Si NO hay errores en los campos, guarda todo---*/
-                users[req.session.user.id -1].firstName = req.body.firstName
-                users[req.session.user.id -1].lastName = req.body.lastName
-                users[req.session.user.id -1].password = bcryptjs.hashSync(req.body.password, 10);
-                users[req.session.user.id -1].email = req.body.email 
+            db.User.update({
+                first_name: req.body.firstName,
+                last_name: req.body.lastName,
+                email: req.body.email,
+                password:  req.body.password,
+            }, {
+                where: {
+                id: {[db.Sequelize.Op.like] : [req.session.user.id]}
+            }})
 
-                passwordConfirmation = true;
+            passwordConfirmation = true;
+
+            /*---Chequea si el Admin Code es correcto, para hacer a ese usuario administrador del sitio---*/
+            if (req.body.adminCode == "sarasa.20") {
+                db.User.update({
+                    role: 'admin'
+                }, {
+                    where: {
+                    id: {[db.Sequelize.Op.like] : [req.session.user.id]}
+                }})
+            } else if (req.body.adminCode != "") {
+                db.User.update({
+                    role: 'user'
+                }, {
+                    where: {
+                    id: {[db.Sequelize.Op.like] : [req.session.user.id]}
+                }})
+            }
+
+            res.render('./users/profile', {passwordConfirmation : passwordConfirmation});
+
+        }   else if (!errors.isEmpty()) { 
+                /*---Si el UNICO error es de la contraseña vacia, guarda los demás datos, pero NO actualiza password---*/
+                db.User.update({
+                    first_name: req.body.firstName,
+                    last_name: req.body.lastName,
+                    email: req.body.email,
+                }, {
+                    where: {
+                    id: {[db.Sequelize.Op.like] : [req.session.user.id]}
+                }})
 
                 /*---Chequea si el Admin Code es correcto, para hacer a ese usuario administrador del sitio---*/
                 if (req.body.adminCode == "sarasa.20") {
-                    users[req.session.user.id -1].adminCode = true;
-                } else if (req.body.adminCode != "") {
-                    users[req.session.user.id -1].adminCode = false;
-                }
-                /*-----Guardamos datos en el users.json-----*/
-                const usersJSON = JSON.stringify(users);
-                fs.writeFileSync(usersDir, usersJSON);
-
-                res.render('./users/profile', {passwordConfirmation : passwordConfirmation});
-
-            } else if (!errors.isEmpty()) { 
-                    /*---Si el UNICO error es de la contraseña vacia, guarda los demás datos, pero NO actualiza password---*/
-                    if (req.body.passwordRepeat == '' && errors.errors.length == 1) {
-                        users[req.session.user.id -1].firstName = req.body.firstName
-                        users[req.session.user.id -1].lastName = req.body.lastName
-                        users[req.session.user.id -1].email = req.body.email 
-
-                        /*---Chequea si el Admin Code es correcto, para hacer a ese usuario administrador del sitio---*/
-                        if (req.body.adminCode == "sarasa.20") {
-                        users[req.session.user.id -1].adminCode = true;
-                        } else if (req.body.adminCode != "") {
-                        users[req.session.user.id -1].adminCode = false;
-                        }
-                        /*-----Guardamos datos en el users.json-----*/
-                        const usersJSON = JSON.stringify(users);
-                        fs.writeFileSync(usersDir, usersJSON);
+                    db.User.update({
+                        role: 'admin'
+                    }, {
+                        where: {
+                        id: {[db.Sequelize.Op.like] : [req.session.user.id]}
+                    }})
+                    } else if (req.body.adminCode != "") {
+                        db.User.update({
+                            role: 'user'
+                        }, {
+                            where: {
+                            id: {[db.Sequelize.Op.like] : [req.session.user.id]}
+                        }})
+                    }
                         
                         res.redirect('/users/profile/');
 
                     /*---Si hay varios errores redirige mostrandolos---*/
-                    } else {
-                        return res.render('./users/profile', {errors: errors.errors});
-                        }
-                    
-                } 
-                    
+            } else {
+                return res.render('./users/profile', {errors: errors.errors});
+            }                    
     },
     photoUpdate: function (req, res, next) {
         /*---Aqui se guarda el nombre del archivo del nuevo avatar---*/
