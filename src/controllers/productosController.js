ba4a3865fc321dbf0bf5a951f6cea7491c3af934
maf248 <maf_248@hotmail.com>
2021-02-08 18:57:25 -0300

    Elimine lineas viejas de codigo del controlador de productos, ya que no se utiliza mas el JSON

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -124,6 +124,7 @@ const productosController = {
       /*----Acá llamamos a la funcion creada, para que al recibir los archivos subidos, guarde sus nombres en imageDir----*/
       uploadFilesDir(req.files);
        /*----Acá guardamos toda la información del producto nuevo----*/
+       
        db.Product.create({
           name: req.body.name,
           type: req.body.type,
@@ -353,6 +354,58 @@ const productosController = {
         }, {include: [{association: 'Sections', include: [{association: 'Contents'}]}, {association: 'Categories'}]})
 
         
+        db.Benefit.bulkCreate({
+          name: req.body.a[0]
+        },{
+          name: req.body.b[0]
+        },{
+          name: req.body.c[0]
+        },{
+          name: req.body.d[0]
+        },{
+          name: req.body.e[0]
+        },{
+          name: req.body.e[0]
+        },{
+          name: req.body.f[0]
+        },{
+          name: req.body.g[0]
+        },{
+          name: req.body.h[0]
+        },{
+          name: req.body.i[0]
+        },{
+          name: req.body.j[0]
+        },{
+          name: req.body.k[0]
+        },{
+          name: req.body.l[0]
+        },{
+          name: req.body.m[0]
+        },{
+          name: req.body.n[0]
+        },{
+          name: req.body.o[0]
+        },{
+          name: req.body.p[0]
+        },{
+          name: req.body.q[0]
+        },{
+          name: req.body.r[0]
+        },{
+          name: req.body.s[0]
+        },{
+          name: req.body.t[0]
+        },{
+          name: req.body.u[0]
+        },{
+          name: req.body.v[0]
+        },{
+          name: req.body.w[0]
+        },{
+          name: req.body.x[0]
+        })
+
           res.redirect('/products');
         
         },
@@ -410,102 +463,19 @@ const productosController = {
         text: imageDir.aicon1
       }, {where: {product_id: req.params.id}})
 
-                    /*----Generales----*/
-    products[req.params.id -1].price = [Number(req.body.price[0]), Number(req.body.price[1]), Number(req.body.price[2])];
-    products[req.params.id -1].category = [req.body.category1, req.body.category2, req.body.category3];
-    products[req.params.id -1].categoryImage = [imageDir.categoryImage1, imageDir.categoryImage2, imageDir.categoryImage3];
-    products[req.params.id -1].description = req.body.description;
-                    /*----Sección 1----*/
-    products[req.params.id -1].seccion1.atitle = req.body.atitle;
-    products[req.params.id -1].seccion1.aicon1 = imageDir.aicon1;
-    products[req.params.id -1].seccion1.asubtitle1 = req.body.asubtitle1;
-    products[req.params.id -1].seccion1.adescription1 = req.body.adescription1;
-    products[req.params.id -1].seccion1.asubtitle2 = req.body.asubtitle2;
-    products[req.params.id -1].seccion1.aicon2 = imageDir.aicon2;
-    products[req.params.id -1].seccion1.adescription2 = req.body.adescription2;
-    products[req.params.id -1].seccion1.asubtitle3 = req.body.asubtitle3;
-    products[req.params.id -1].seccion1.aicon3 = imageDir.aicon3;
-    products[req.params.id -1].seccion1.adescription3 = req.body.adescription3;
-    products[req.params.id -1].seccion1.asubtitle4 = req.body.asubtitle4;
-    products[req.params.id -1].seccion1.adescription4 = req.body.adescription4;
-    products[req.params.id -1].seccion1.aicon4 = imageDir.aicon4;
-    products[req.params.id -1].seccion1.aimage = imageDir.aimage;
-                    /*----Sección 2----*/
-    products[req.params.id -1].seccion2.btitle = req.body.btitle;
-    products[req.params.id -1].seccion2.bicon1 = imageDir.bicon1;
-    products[req.params.id -1].seccion2.bsubtitle1 = req.body.bsubtitle1;
-    products[req.params.id -1].seccion2.bdescription1 = req.body.bdescription1;
-    products[req.params.id -1].seccion2.bsubtitle2 = req.body.bsubtitle2;
-    products[req.params.id -1].seccion2.bicon2 = imageDir.bicon2;
-    products[req.params.id -1].seccion2.bdescription2 = req.body.bdescription2;
-    products[req.params.id -1].seccion2.bsubtitle3 = req.body.bsubtitle3;
-    products[req.params.id -1].seccion2.bicon3 = imageDir.bicon3;
-    products[req.params.id -1].seccion2.bdescription3 = req.body.bdescription3;
-    products[req.params.id -1].seccion2.bsubtitle4 = req.body.bsubtitle4;
-    products[req.params.id -1].seccion2.bicon4 = imageDir.bicon4;
-    products[req.params.id -1].seccion2.bdescription4 = req.body.bdescription4;
-    products[req.params.id -1].seccion2.bimage = imageDir.bimage;
-                    /*----Sección 3----*/
-    products[req.params.id -1].seccion3.ctitle = req.body.ctitle;
-    products[req.params.id -1].seccion3.cicon1 = imageDir.cicon1;
-    products[req.params.id -1].seccion3.csubtitle1 = req.body.csubtitle1;
-    products[req.params.id -1].seccion3.cdescription1 = req.body.cdescription1;
-    products[req.params.id -1].seccion3.csubtitle2 = req.body.csubtitle2;
-    products[req.params.id -1].seccion3.cicon2 = imageDir.cicon2;
-    products[req.params.id -1].seccion3.cdescription2 = req.body.cdescription2;
-    products[req.params.id -1].seccion3.csubtitle3 = req.body.csubtitle3;
-    products[req.params.id -1].seccion3.cicon3 = imageDir.cicon3;
-    products[req.params.id -1].seccion3.cdescription3 = req.body.cdescription3;
-    products[req.params.id -1].seccion3.csubtitle4 = req.body.csubtitle4;
-    products[req.params.id -1].seccion3.cicon4 = imageDir.cicon4;
-    products[req.params.id -1].seccion3.cdescription4 = req.body.cdescription4;
-    products[req.params.id -1].seccion3.cimage = imageDir.cimage;
-                    /*----Sección 4----*/
-    products[req.params.id -1].seccion4.dtitle = req.body.dtitle;
-    products[req.params.id -1].seccion4.dicon1 = imageDir.dicon1;
-    products[req.params.id -1].seccion4.dsubtitle1 = req.body.dsubtitle1;
-    products[req.params.id -1].seccion4.ddescription1 = req.body.ddescription1;
-    products[req.params.id -1].seccion4.dsubtitle2 = req.body.dsubtitle2;
-    products[req.params.id -1].seccion4.dicon2 = imageDir.dicon2;
-    products[req.params.id -1].seccion4.ddescription2 = req.body.ddescription2;
-    products[req.params.id -1].seccion4.dsubtitle3 = req.body.dsubtitle3;
-    products[req.params.id -1].seccion4.dicon3 = imageDir.dicon3;
-    products[req.params.id -1].seccion4.ddescription3 = req.body.ddescription3;
-    products[req.params.id -1].seccion4.dsubtitle4 = req.body.dsubtitle4;
-    products[req.params.id -1].seccion4.dicon4 = imageDir.dicon4;
-    products[req.params.id -1].seccion4.ddescription4 = req.body.ddescription4;
-    products[req.params.id -1].seccion4.dimage = imageDir.dimage;
-                    /*----Sección 5----*/
-    products[req.params.id -1].seccion5.etitle = req.body.etitle;
-    products[req.params.id -1].seccion5.eicon1 = imageDir.eicon1;
-    products[req.params.id -1].seccion5.esubtitle1 = req.body.esubtitle1;
-    products[req.params.id -1].seccion5.edescription1 = req.body.edescription1;
-    products[req.params.id -1].seccion5.esubtitle2 = req.body.esubtitle2;
-    products[req.params.id -1].seccion5.eicon2 = imageDir.eicon2;
-    products[req.params.id -1].seccion5.edescription2 = req.body.edescription2;
-    products[req.params.id -1].seccion5.esubtitle3 = req.body.esubtitle3;
-    products[req.params.id -1].seccion5.eicon3 = imageDir.eicon3;
-    products[req.params.id -1].seccion5.edescription3 = req.body.edescription3;
-    products[req.params.id -1].seccion5.esubtitle4 = req.body.esubtitle4;
-    products[req.params.id -1].seccion5.eicon4 = imageDir.eicon4;
-    products[req.params.id -1].seccion5.edescription4 = req.body.edescription4;
-    products[req.params.id -1].seccion5.eimage = imageDir.eimage;
-                /*----Benefits----*/
-    products[req.params.id -1].benefits.costoTransaccion = ["Costo por transacción", req.body.costoTransaccion[1], req.body.costoTransaccion[2], req.body.costoTransaccion[3]]
-    products[req.params.id -1].benefits.cantidadSecciones = ["Cantidad de secciones", req.body.cantidadSecciones1 , req.body.cantidadSecciones2, req.body.cantidadSecciones3]
-
+    /*
     for (let i = 0; i < indexBenefits.length; i++) {
       if (req.body[indexBenefits[i]][0] != "") {
         products[products.length -1].benefits[indexBenefits[i]] = [req.body[indexBenefits[i]][0], req.body[indexBenefits[i]][1], req.body[indexBenefits[i]][2], req.body[indexBenefits[i]][3]];
         }
       }
-
+    */
     /*----Acá termina la actualización de datos de formularios, en la variable products----*/
 
     /*----Guardando imagen nueva y cambios en productos data base----*/
+    /*
 		products[req.params.id -1].image = imageDir.image;
-		const productsJSON = JSON.stringify(products);
-		fs.writeFileSync(productsDir, productsJSON);
+		*/
 		res.redirect('/products');
     },
     borrado: function(req, res, next) {
