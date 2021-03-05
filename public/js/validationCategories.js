b920fb57031e24c800c6e8f1b3dc459481e9346f
maf248 <maf_248@hotmail.com>
2021-03-05 11:46:03 -0300

    Automatice la validacion por front para simplificar el codigo, usando queryselectorall en los nombres de categorías. Falta implementarlo en los demas campos de la misma manera.

--- public/js/validationCategories.js
+++ public/js/validationCategories.js
@@ -17,84 +17,101 @@ const price1 = qs('#price1');
 const price2 = qs('#price2');
 const price3 = qs('#price3');
 
+const categoryNames = qsa('#categoryNames')
+console.log(categoryNames);
+let messageToShow = qsa(`#categoryNameError`)
+
 const formToEdit = qs('#formToEdit');
 const formToCreate = qs('#formToCreate');
 
 const formButton = qs('#formButton');
-// <div id="category1NameError"></div>
-//<div id="categoryImage3Error"></div>
-//<div id="transactionCost1Error"></div>
-//<div id="sections1Error"></div>
-//<div id="price1Error"></div>
-let productNameOK = false;
-let productImageOK = true;
-let bannerTitleOK = false;
-let bannerSubtitleOK = false;
+
+// let productNameOK = false;
+// let productImageOK = true;
+// let bannerTitleOK = false;
+// let bannerSubtitleOK = false;
+
+for(let i=0; i < categoryNames.length; i++) {   
+    categoryNames[i].addEventListener('change', (e) => {
+        if (categoryNames[i].value.length < 3) {
+            
+            messageToShow[i].innerHTML = `<p> Debe contener al menos 3 caracteres </p>`
+            messageToShow[i].style.textAlign = "center"
+            messageToShow[i].style.color = "red"
+            messageToShow[i].style.margin = "15px"
+            messageToShow[i].style.fontSize = "0.5em"
+    
+        } else {
+            messageToShow[i].innerHTML = "" 
+        }
+    })
+ 
+}
+
+
+
+categoryImage1.addEventListener('change', (e) => {
+    if (categoryImage1.value.split('.')[1] != "jpg" && categoryImage1.value.split('.')[1] != "jpeg" && categoryImage1.value.split('.')[1] != "png") {   
+        let messageToShow = qs('#categoryImage1Error')
+        messageToShow.innerHTML = `<p> La imagen debe estar en formato JPG, JPEG o PNG </p>`
+        messageToShow.style.textAlign = "center"
+        messageToShow.style.color = "red"
+        messageToShow.style.margin = "15px"
+        messageToShow.style.fontSize = "0.5em"
+
+    } else {
+        let messageToShow = qs('#categoryImage1Error')
+        messageToShow.innerHTML = "" 
+        
+    }
+})
+transactionCost1.addEventListener('change', (e) => {
+    if (transactionCost1.value < 0 || transactionCost1.value > 100) {
+        let messageToShow = qs('#transactionCost1Error')
+        messageToShow.innerHTML = `<p> Debe ingresar un numero entre 0 y 100 </p>`
+        messageToShow.style.textAlign = "center"
+        messageToShow.style.color = "red"
+        messageToShow.style.margin = "15px"
+        messageToShow.style.fontSize = "0.5em"
+
+    } else {
+        let messageToShow = qs('#transactionCost1Error')
+        messageToShow.innerHTML = "" 
+    }
+})
+
+sections1.addEventListener('change', (e) => {
+    if (sections1.value < 1 || sections1.value > 500) {
+        let messageToShow = qs('#sections1Error')
+        messageToShow.innerHTML = `<p> Debe ingresar un numero entre 1 y 500 </p>`
+        messageToShow.style.textAlign = "center"
+        messageToShow.style.color = "red"
+        messageToShow.style.margin = "15px"
+        messageToShow.style.fontSize = "0.5em"
+
+    } else {
+        let messageToShow = qs('#sections1Error')
+        messageToShow.innerHTML = "" 
+    }
+})
+
+price1.addEventListener('change', (e) => {
+    if (price1.value < 0 || price1.value > 16777215) {
+        let messageToShow = qs('#price1Error')
+        messageToShow.innerHTML = `<p> Debe ingresar un numero mayor o igual a 0 </p>`
+        messageToShow.style.textAlign = "center"
+        messageToShow.style.color = "red"
+        messageToShow.style.margin = "15px"
+        messageToShow.style.fontSize = "0.5em"
+
+    } else {
+        let messageToShow = qs('#price1Error')
+        messageToShow.innerHTML = "" 
+    }
+})
+
 
 if (window.location.pathname.includes('edit')) {
-    productName.addEventListener('change', (e) => {
-        if (productName.value.length < 3) {
-            let messageToShow = qs('#productNameError')
-            messageToShow.innerHTML = `<p> Debe contener al menos 3 caracteres </p>`
-            messageToShow.style.textAlign = "center"
-            messageToShow.style.color = "red"
-            messageToShow.style.margin = "15px"
-            productNameOK = false;
-    
-        } else {
-            let messageToShow = qs('#productNameError')
-            messageToShow.innerHTML = "" 
-            productNameOK = true;
-        }
-    })
-    
-    productImage.addEventListener('change', (e) => {
-        if (productImage.value.split('.')[1] != "jpg" && productImage.value.split('.')[1] != "jpeg" && productImage.value.split('.')[1] != "png") {   
-            let messageToShow = qs('#productImageError')
-            messageToShow.innerHTML = `<p> La imagen debe estar en formato JPG, JPEG o PNG </p>`
-            messageToShow.style.textAlign = "center"
-            messageToShow.style.color = "red"
-            messageToShow.style.margin = "15px"
-            productImageOK = false
-
-        } else {
-            let messageToShow = qs('#productImageError')
-            messageToShow.innerHTML = "" 
-            productImageOK = true
-            
-        }
-    })
-    bannerTitle.addEventListener('change', (e) => {
-        if (bannerTitle.value.length < 3) {
-            let messageToShow = qs('#bannerTitleError')
-            messageToShow.innerHTML = `<p> Debe contener al menos 3 caracteres </p>`
-            messageToShow.style.textAlign = "center"
-            messageToShow.style.color = "red"
-            messageToShow.style.margin = "15px"
-            bannerTitleOK = false;
-    
-        } else {
-            let messageToShow = qs('#bannerTitleError')
-            messageToShow.innerHTML = "" 
-            bannerTitleOK = true;
-        }
-    })
-
-    bannerSubtitle.addEventListener('change', (e) => {
-        if (bannerSubtitle.value.length < 3) {
-            let messageToShow = qs('#bannerSubtitleError')
-            messageToShow.innerHTML = `<p> Debe contener al menos 3 caracteres </p>`
-            messageToShow.style.textAlign = "center"
-            messageToShow.style.color = "red"
-            messageToShow.style.margin = "15px"
-            bannerSubtitleOK = false;
-    
-        } else {
-            let messageToShow = qs('#bannerSubtitleError')
-            messageToShow.innerHTML = "" 
-            bannerSubtitleOK = true;
-        }
-    })
 
     formToEdit.addEventListener('submit', (e) => {
 
@@ -105,71 +122,9 @@ if (window.location.pathname.includes('edit')) {
         }
     
     })
+
 } else {
-    productName.addEventListener('change', (e) => {
-        if (productName.value.length < 3) {
-            let messageToShow = qs('#productNameError')
-            messageToShow.innerHTML = `<p> Debe contener al menos 3 caracteres </p>`
-            messageToShow.style.textAlign = "center"
-            messageToShow.style.color = "red"
-            messageToShow.style.margin = "15px"
-            productNameOK = false;
     
-        } else {
-            let messageToShow = qs('#productNameError')
-            messageToShow.innerHTML = "" 
-            productNameOK = true;
-        }
-    })
-    
-    productImage.addEventListener('change', (e) => {
-        if (productImage.value.split('.')[1] != "jpg" && productImage.value.split('.')[1] != "jpeg" && productImage.value.split('.')[1] != "png") {   
-            let messageToShow = qs('#productImageError')
-            messageToShow.innerHTML = `<p> La imagen debe estar en formato JPG, JPEG o PNG </p>`
-            messageToShow.style.textAlign = "center"
-            messageToShow.style.color = "red"
-            messageToShow.style.margin = "15px"
-            productImageOK = false
-
-        } else {
-            let messageToShow = qs('#productImageError')
-            messageToShow.innerHTML = "" 
-            productImageOK = true
-            
-        }
-    })
-    bannerTitle.addEventListener('change', (e) => {
-        if (bannerTitle.value.length < 3) {
-            let messageToShow = qs('#bannerTitleError')
-            messageToShow.innerHTML = `<p> Debe contener al menos 3 caracteres </p>`
-            messageToShow.style.textAlign = "center"
-            messageToShow.style.color = "red"
-            messageToShow.style.margin = "15px"
-            bannerTitleOK = false;
-    
-        } else {
-            let messageToShow = qs('#bannerTitleError')
-            messageToShow.innerHTML = "" 
-            bannerTitleOK = true;
-        }
-    })
-
-    bannerSubtitle.addEventListener('change', (e) => {
-        if (bannerSubtitle.value.length < 3) {
-            let messageToShow = qs('#bannerSubtitleError')
-            messageToShow.innerHTML = `<p> Debe contener al menos 3 caracteres </p>`
-            messageToShow.style.textAlign = "center"
-            messageToShow.style.color = "red"
-            messageToShow.style.margin = "15px"
-            bannerSubtitleOK = false;
-    
-        } else {
-            let messageToShow = qs('#bannerSubtitleError')
-            messageToShow.innerHTML = "" 
-            bannerSubtitleOK = true;
-        }
-    })
-
     formToCreate.addEventListener('submit', (e) => {
 
         if(productNameOK == false || productImageOK == false || bannerTitleOK == false || bannerSubtitleOK == false) {
