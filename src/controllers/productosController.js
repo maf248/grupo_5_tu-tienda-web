beea804506e859379c1dca9db537b6464eef1d50
maf248 <maf_248@hotmail.com>
2021-03-06 14:18:07 -0300

    Cambie la redireccion al finalizar la edicion de beneficios, para que vuelva a la misma etapa del proceso pero con los datos guardados

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -635,7 +635,7 @@ const productosController = {
 
                         }
 
-                        res.redirect(`/products/${product.id}`)
+                        res.redirect(`/products/${product.id}/edit/benefits `)
                     })
 
             })
