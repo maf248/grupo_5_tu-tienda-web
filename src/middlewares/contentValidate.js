182e19644e9c12d1a4ea58bc6b319156ab442f86
maf248 <maf_248@hotmail.com>
2021-03-08 14:51:12 -0300

    Mejoré la validacion backend de Contents, funciona perfectamente al crear un contenido nuevo, falta ajustarlo para que funcione correctamente al editar contenidos ya existentes.

--- src/middlewares/contentValidate.js
+++ src/middlewares/contentValidate.js
@@ -9,21 +9,25 @@ module.exports = [
     check('contentSubtitle')
     .isLength({min:3})
     .withMessage('Este campo debe contener al menos 3 carateres'),
-    check('contentsDescription')
+    check('contentDescription')
     .isLength({min:10})
     .withMessage('Este campo debe contener al menos 10 carateres'),
     body('contentIcon')
     .custom(function(value, {req}) {
-        if (req.params.id) {
+
+        if (req.url.includes('create')) {
             if (typeof req.files[0] != 'undefined') {
 
                 var extension = (path.extname(req.files[0].filename)).toLowerCase();
                 switch (extension) {
                 case '.svg':
                     return true;
+                default:
+                    return false;
                 }
+
             }
-            return true;
+            return false;
 
         } else {
  
@@ -33,9 +37,11 @@ module.exports = [
                 switch (extension) {
                 case '.svg':
                     return true;
+                default:
+                    return false;
                 }
             }
-            return false;
+            return true;
         }
     })
     .withMessage('Debe subir una imagen en formato svg')
