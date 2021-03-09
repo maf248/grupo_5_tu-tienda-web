b87678c485ad18477ada025c82e4ad0de257b5de
maf248 <maf_248@hotmail.com>
2021-03-09 19:35:07 -0300

    Completé la validacion por FRONTEND de CONTENTS, para que funcione correctamente en ambos casos (editando, o creando)

--- public/js/validationContents.js
+++ public/js/validationContents.js
@@ -26,15 +26,13 @@ contentToShowIcon[i].style.display = 'block'
 
 }
 
-
 //Acá comienza la lógica para validar los imputs
-
 
 //Acá creamos variables que luego serán utilizadas para hacer o no el preventDefault en el formulario de edición o creación.
 
-let variable1 = true;
-let variable2 = false;
-let variable3 = false;
+let iconsVerifications = [false];
+let subtitlesVerifications = [false];
+let descriptionsVerifications = [false];
 
 
 if (window.location.pathname.includes('edit')) {
@@ -43,98 +41,91 @@ if (window.location.pathname.includes('edit')) {
 
     // Acá seleccionamos los inputs y formularios correspondientes para trabajar en la EDICIÓN de contenidos.
 
-    const editIconInput = qs('#iconToEdit');
-    const formToEditIcon = qs('#formToEditIcon');
+    const editIconInputs = qsa('#iconToEdit');
+    const formToEditIcons = qsa('#formToEditIcon');
+    const errorMessageIcons = qsa('#editIconContainer');
 
-    const editSubtitleInput = qs('#subtitleToEdit');
-    const formToEditSubtitle = qs('#formToEditSubtitle');
+    const editSubtitleInputs = qsa('#subtitleToEdit');
+    const formToEditSubtitles = qsa('#formToEditSubtitle');
+    const errorMessageSubtitles = qsa('#editSubtitleContainer')
 
-    const editDescriptionInput = qs('#descriptionToEdit');
-    const formToEditDescription = qs('#formToEditDescription');
+    const editDescriptionInputs = qsa('#descriptionToEdit');
+    const formToEditDescriptions = qsa('#formToEditDescription');
+    const errorMessageDescriptions = qsa('#editDescriptionContainer')
+   
+    for (let i=0; i < editIconInputs.length; i++) {
+        editIconInputs[i].addEventListener('change', (e) => {
+            if (editIconInputs[i].value.split('.')[1] != "svg") {   
+                errorMessageIcons[i].innerHTML = `<p> La imagen debe estar en formato SVG </p>`
+                errorMessageIcons[i].style.textAlign = "center"
+                errorMessageIcons[i].style.color = "red"
+                errorMessageIcons[i].style.margin = "15px"
+                iconsVerifications[i] = false;
 
-    editIconInput.addEventListener('change', (e) => {
-        if (editIconInput.value.split('.')[1] != "svg") {   
-            let messageToShow = qs('#editIconContainer')
-            messageToShow.innerHTML = `<p> La imagen debe estar en formato SVG </p>`
-            messageToShow.style.textAlign = "center"
-            messageToShow.style.color = "red"
-            messageToShow.style.margin = "15px"
-            variable1 = false
+            } else {
+                errorMessageIcons[i].innerHTML = "" 
+                iconsVerifications[i] = true;
+                
+            }
+        })
+        formToEditIcons[i].addEventListener('submit', (e) => {
 
-        } else {
-            let messageToShow = qs('#editIconContainer')
-            messageToShow.innerHTML = "" 
-            variable1 = true
-            
-        }
-    })
-
-    formToEditIcon.addEventListener('submit', (e) => {
-
-        if( variable1 == false) {
+            if(iconsVerifications[i] == false) {
+        
+                e.preventDefault()
+        
+            }
+        
+        })
+    }
     
-            e.preventDefault()
-    
-        }
-    
-    })
+    for (let i=0; i < editSubtitleInputs.length; i++) {
+        editSubtitleInputs[i].addEventListener('change', (e) => {
+            if (editSubtitleInputs[i].value.length < 3) {
+                errorMessageSubtitles[i].innerHTML = `<p> El título debe contener al menos 3 caracteres </p>`
+                errorMessageSubtitles[i].style.textAlign = "center"
+                errorMessageSubtitles[i].style.color = "red"
+                errorMessageSubtitles[i].style.margin = "15px"
+                subtitlesVerifications[i] = false;
 
-    editSubtitleInput.addEventListener('change', (e) => {
-        if (editSubtitleInput.value.length < 3) {
-            let messageToShow = qs('#editSubtitleContainer')
-            messageToShow.innerHTML = `<p> El título debe contener al menos 3 caracteres </p>`
-            messageToShow.style.textAlign = "center"
-            messageToShow.style.color = "red"
-            messageToShow.style.margin = "15px"
-            variable2 = false;
+        
+            } else {
+                errorMessageSubtitles[i].innerHTML = "" 
+                subtitlesVerifications[i] = true;
+            }
+        })
+        formToEditSubtitles[i].addEventListener('submit', (e) => {
 
-    
-        } else {
-            let messageToShow = qs('#editSubtitleContainer')
-            messageToShow.innerHTML = "" 
-            variable2 = true;
-        }
-    })
+            if(subtitlesVerifications[i] == false) {
+                e.preventDefault()
+            }
+        
+        })
+    }
 
-    formToEditSubtitle.addEventListener('submit', (e) => {
+    for (let i=0; i < editDescriptionInputs.length; i++) {
+        editDescriptionInputs[i].addEventListener('change', (e) => {
+            if (editDescriptionInputs[i].value.length < 10) {
+                errorMessageDescriptions[i].innerHTML = `<p> El título debe contener al menos 10 caracteres </p>`
+                errorMessageDescriptions[i].style.textAlign = "center"
+                errorMessageDescriptions[i].style.color = "red"
+                errorMessageDescriptions[i].style.margin = "15px"
+                descriptionsVerifications[i] = false;
 
-        if( variable2 == false) {
-    
-            e.preventDefault()
-    
-        }
-    
-    })
+        
+            } else {
+                errorMessageDescriptions[i].innerHTML = "" 
+                descriptionsVerifications[i] = true;
+            }
+        })
+        formToEditDescriptions[i].addEventListener('submit', (e) => {
 
-    editDescriptionInput.addEventListener('change', (e) => {
-        if (editDescriptionInput.value.length < 10) {
-            let messageToShow = qs('#editDescriptionContainer')
-            messageToShow.innerHTML = `<p> El título debe contener al menos 10 caracteres </p>`
-            messageToShow.style.textAlign = "center"
-            messageToShow.style.color = "red"
-            messageToShow.style.margin = "15px"
-            variable3 = false;
-
-    
-        } else {
-            let messageToShow = qs('#editDescriptionContainer')
-            messageToShow.innerHTML = "" 
-            variable3 = true;
-        }
-    })
-    
-
-
-    formToEditDescription.addEventListener('submit', (e) => {
-
-        if( variable3 == false) {
-    
-            e.preventDefault()
-    
-        }
-    
-    })
-
+            if(descriptionsVerifications[i] == false) {
+                e.preventDefault()
+            }
+        
+        })
+    }
 
 } else {
 
@@ -158,12 +149,12 @@ if (window.location.pathname.includes('edit')) {
             messageToShow.style.textAlign = "center"
             messageToShow.style.color = "red"
             messageToShow.style.margin = "15px"
-            variable1 = false
+            iconsVerifications[0] = false;
 
         } else {
             let messageToShow = qs('#containerOfCreateIconValidation')
             messageToShow.innerHTML = "" 
-            variable1 = true
+            iconsVerifications[0] = true;
             
         }
     })
@@ -175,13 +166,12 @@ if (window.location.pathname.includes('edit')) {
             messageToShow.style.textAlign = "center"
             messageToShow.style.color = "red"
             messageToShow.style.margin = "15px"
-            variable2 = false;
+            subtitlesVerifications[0] = false;
 
-    
         } else {
             let messageToShow = qs('#containerOfCreateSubtitleValidation')
             messageToShow.innerHTML = "" 
-            variable2 = true;
+            subtitlesVerifications[0] = true;
         }
     })
 
@@ -192,34 +182,19 @@ if (window.location.pathname.includes('edit')) {
             messageToShow.style.textAlign = "center"
             messageToShow.style.color = "red"
             messageToShow.style.margin = "15px"
-            variable3 = false;
+            descriptionsVerifications[0] = false;
 
-    
         } else {
             let messageToShow = qs('#containerOfCreateDescriptionValidation')
             messageToShow.innerHTML = "" 
-            variable3 = true;
+            descriptionsVerifications[0] = true;
         }
     })
     
 
-
     formToCreateContents.addEventListener('submit', (e) => {
 
-        if (createIconInput.value == "") {
-            let messageToShow = qs('#containerOfCreateNoIconValidation')
-            messageToShow.innerHTML = `<p>Debés subir una imagen </p>`
-            messageToShow.style.textAlign = "center"
-            messageToShow.style.color = "red"
-            messageToShow.style.margin = "15px"
-            e.preventDefault()
-        }
-        if (createIconInput.value != "") {
-            let messageToShow = qs('#containerOfCreateNoIconValidation')
-            messageToShow.innerHTML = ""
-        }
-
-        if( variable1 == false || variable2 == false || variable3 == false ) {
+        if(iconsVerifications[0] == false || subtitlesVerifications[0] == false || descriptionsVerifications[0] == false ) {
     
             e.preventDefault()
     
